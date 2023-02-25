ee2b856f13c5c114bad062615e6d24e0710d48f5
Ajay84sia <ajay9679339@gmail.com>
2023-02-25 18:54:18 +0530

    submitAdminUI

--- my-app/src/AdminPages/AdminDashboard.jsx
+++ my-app/src/AdminPages/AdminDashboard.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import AdminFooter from './AdminFooter'
-import AdminNavbar from './AdminNavbar'
+import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import React from "react";
+import AdminFooter from "./AdminFooter";
+import AdminNavbar from "./AdminNavbar";
 
 const AdminDashboard = () => {
   return (
     <div>
-        <AdminNavbar/>
-        <h1>Hello Admin</h1>
-        <AdminFooter/>      
-    </div>
-  )
-}
+      <AdminNavbar />
+      <Box py={20} px={5}>
+        <Tabs orientation='vertical' variant='solid-rounded'>
+          <TabList  width='25%' boxShadow='md' py={5} px={4} borderRadius='10px'>
+            <Tab height='50px'>Add Products</Tab>
+            <Tab height='50px'>Remove Products</Tab>
+            <Tab height='50px'>Change Product Details</Tab>
+            <Tab height='50px'>Quantity Management</Tab>
+            <Tab height='50px'>Order Management</Tab>
+          </TabList>
 
-export default AdminDashboard
+          <TabPanels>
+            <TabPanel>
+              <p>Add Products</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Remove Products</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Change Details</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Manage Quantity</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Manage Order</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+      <AdminFooter />
+    </div>
+  );
+};
+
+export default AdminDashboard;
--- my-app/src/AdminPages/AdminLogin.jsx
+++ my-app/src/AdminPages/AdminLogin.jsx
@@ -1,78 +1,140 @@
 import {
-    Flex,
-    Box,
-    FormControl,
-    FormLabel,
-    Input,
-    Checkbox,
-    Stack,
-    Link,
-    Button,
-    Heading,
-    Text,
-    useColorModeValue,
-  } from "@chakra-ui/react";
-  import Footer from "./HomePage/Footer";
-  import NewNavbar from "./HomePage/NewNavbar";
-  
-  export default function Login() {
-    return (
-      <>
-        <NewNavbar />
-        <Flex
-          minH={"100vh"}
-          align={"center"}
-          justify={"center"}
-          paddingTop="25px"
-          bg={useColorModeValue("gray.50", "gray.800")}
-        >
-          <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-            <Stack align={"center"}>
-              <Heading fontSize={"4xl"}>Sign in to your account</Heading>
-              <Text fontSize={"lg"} color={"gray.600"}>
-                to grab the amazing deals 🛍️
-              </Text>
-            </Stack>
-            <Box
-              rounded={"lg"}
-              bg={useColorModeValue("white", "gray.700")}
-              boxShadow={"lg"}
-              p={8}
-            >
-              <Stack spacing={4}>
-                <FormControl id="email">
-                  <FormLabel>Email address</FormLabel>
-                  <Input type="email" />
-                </FormControl>
-                <FormControl id="password">
-                  <FormLabel>Password</FormLabel>
-                  <Input type="password" />
-                </FormControl>
-                <Stack spacing={10}>
-                  <Stack
-                    direction={{ base: "column", sm: "row" }}
-                    align={"start"}
-                    justify={"space-between"}
-                  >
-                    <Checkbox>Remember me</Checkbox>
-                    <Link color={"blue.400"}>Forgot password?</Link>
-                  </Stack>
-                  <Button
-                    bg={"blue.400"}
-                    color={"white"}
-                    _hover={{
-                      bg: "blue.500",
-                    }}
-                  >
-                    Sign in
-                  </Button>
-                </Stack>
-              </Stack>
-            </Box>
-          </Stack>
-        </Flex>
-        <Footer />
-      </>
-    );
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  useToast,
+} from "@chakra-ui/react";
+import {  useContext, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { AdminAuthContext } from "./AdminAuthContext";
+// import { Navigate } from "react-router-dom";
+import AdminFooter from "./AdminFooter";
+import AdminNavbar from "./AdminNavbar";
+
+
+export default function Login() {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const {adminAuth, adminlogin} = useContext(AdminAuthContext)
+  const color1 = useColorModeValue("gray.50", "gray.800")
+  const color2 = useColorModeValue("white", "gray.700")
+  const toast = useToast();
+
+  const handlepostData = (data) => {
+    fetch("https://reqres.in/api/login", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data.token);
+        adminlogin()
+        toast({
+          title: `Sign in Successful`,
+          status: "success",
+          duration: 1000,
+          isClosable: true,
+        })
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  if(adminAuth){
+    return <Navigate to='/admindash' />
   }
-  
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handlepostData(formData);
+  };
+
+  const { email, password } = formData;
+  return (
+    <>
+      <AdminNavbar />
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        paddingTop="25px"
+        bg={color1}
+      >
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"}>Admin Login</Heading>
+            <Text fontSize={"lg"} color={"gray.600"}>
+              Login to access Admin Dashboard
+            </Text>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            bg={color2}
+            boxShadow={"lg"}
+            p={8}
+          >
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: "column", sm: "row" }}
+                  align={"start"}
+                  justify={"space-between"}
+                >
+                  <Checkbox>Remember me</Checkbox>
+                  <Link color={"blue.400"}>Forgot password?</Link>
+                </Stack>
+                <Button
+                  bg={"blue.400"}
+                  color={"white"}
+                  onClick={handleSubmit}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                >
+                  Sign in
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+      <AdminFooter/>
+    </>
+  );
+}
